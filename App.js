4369152c2daffa43ff4e913f046bb9c216c8c5c2
Doug <dougbacelar@gmail.com>
2017-10-25 19:36:49 +0100

    implementing asyncstorage

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 import './global';
 
 const Web3 = require('web3');
@@ -13,8 +13,28 @@ const styles = StyleSheet.create({
   },
 });
 
+async function test() {
+  const myAddresses = {
+    eth: ['1234567', 'abscasdqwe'],
+    btc: ['654321'],
+  };
+
+  try {
+    console.log('trying to save address');
+    return AsyncStorage.setItem(
+      'addresses',
+      JSON.stringify(myAddresses), // JSON.stringify(myAddresses)
+      // err => (err ? console.log('error') : console.log('saved addresses')),
+    );
+  } catch (err) {
+    throw Error(`something went wrong yo${err}`);
+  }
+}
+
 export default class App extends React.Component {
   componentWillMount() {
+    test().then(AsyncStorage.getItem('addresses').then(console.log));
+
     const web3 = new Web3(
       new Web3.providers.HttpProvider('https://mainnet.infura.io/'),
     );
